2383f5aa6e3d44c17d20af936b97963bf5b24c93
Marin-univ <144822096+Marin-univ@users.noreply.github.com>
2024-04-09 14:39:15 +0200

    Add files via upload

---
+++ requetes.py
@@ -0,0 +1,35 @@
+# Q1
+def json_vers_nx(chemin):
+    pass
+
+# Q2
+def collaborateurs_communs(G,u,v):
+    pass
+
+# Q3
+def collaborateurs_proches(G,u,k):
+    pass
+
+def est_proche(G,u,v,k=1):
+    pass
+
+def distance_naive(G,u,v):
+    pass
+
+def distance(G,u,v):
+    pass
+
+# Q4
+def centralite(G,u):
+    pass
+
+def centre_hollywood(G):
+    pass
+
+# Q5
+def eloignement_max(G:nx.Graph):
+    pass
+
+# Bonus
+def centralite_groupe(G,S):
+    pass
